f0510c40a684f39227c52f1135b6d4c7fa4542a4
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-07-08 17:02:59 +0300

    structure wip

---
+++ api/ac/export.h
@@ -0,0 +1,15 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include <splat/symbol_export.h>
+
+#if AC_API_SHARED
+#   if BUILDING_AC_API
+#       define AC_API_EXPORT SYMBOL_EXPORT
+#   else
+#       define AC_API_EXPORT SYMBOL_IMPORT
+#   endif
+#else
+#   define AC_API_EXPORT
+#endif
