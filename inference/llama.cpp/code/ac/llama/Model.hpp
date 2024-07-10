8b8790c405206fe6345222789c5ddc308cf8154c
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-07-10 17:52:48 +0300

    llama: some structure, ref #3

---
+++ inference/llama.cpp/code/ac/llama/Model.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include "export.h"
+
+namespace ac::llama {
+class AC_LLAMA_EXPORT Model {
+public:
+    Model();
+    ~Model();
+};
+} // namespace ac::llama
