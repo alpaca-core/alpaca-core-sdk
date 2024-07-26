2ae0221fef750beb9842049f28bbe63f9dfaf970
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-07-26 09:49:00 +0300

    llama: begin api integration, ref #18

---
+++ inference/llama.cpp/local/code/ac/LocalLLama.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+#include "LocalLlama.hpp"
+#include <ac/llama/Model.hpp>
+
+namespace ac::local {
+
+namespace {
+
+}
+
+void addLocalLlamaInference() {
+
+}
+
+} // namespace ac::local
