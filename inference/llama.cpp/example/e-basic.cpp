3351f20c9c4543496cf7482f6a4581529ea92381
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-07-29 16:52:52 +0300

    llama: trivial cli example, ref #24

---
+++ inference/llama.cpp/example/e-basic.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+
+// trivial example of using alpaca-core's llama inference
+
+// llama
+#include <ac/llama/Init.hpp>
+#include <ac/llama/Model.hpp>
+#include <ac/llama/Instance.hpp>
+
+// logging
+#include <jalog/Instance.hpp>
+#include <jalog/sinks/ColorSink.hpp>
+
+// model source directory
+#include "ac-test-data-llama-dir.h"
+
+#include <iostream>
+#include <string>
+
+int main() try {
+    jalog::Instance jl;
+    jl.setup().add<jalog::sinks::ColorSink>();
+
+    // initialize the library
+    ac::llama::initLibrary();
+
+    // load model
+    std::string modelGguf = AC_TEST_DATA_LLAMA_DIR "/gpt2-117m-q6_k.gguf";
+    ac::llama::Model model(modelGguf.c_str(), {});
+
+    // create inference instance
+    ac::llama::Instance instance(model, {});
+
+    std::string prompt = "The first person to";
+    std::cout << "Prompt: " << prompt << "\n";
+
+    // start session
+    auto session = instance.newSession(prompt, {});
+
+    // generate and print 100 tokens
+    for (int i = 0; i < 100; ++i) {
+        auto token = session.getToken();
+        if (token == ac::llama::Token_Invalid) {
+            // no more tokens
+            break;
+        }
+        std::cout << model.vocab().tokenToString(token);
+    }
+    std::cout << '\n';
+
+    return 0;
+}
+catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+}
