9cf0114cf07158b10942d8a245e541146d808f7a
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-07-29 09:49:34 +0300

    minor: sandbox fixes

--- poc/sandbox/sandbox.cpp
+++ poc/sandbox/sandbox.cpp
@@ -78,7 +78,7 @@ int main() {
 
             auto model = result.value();
             model->createInstance("xxx", {}, {
-                [](ac::CallbackResult<ac::InstancePtr> result) {
+                [model](ac::CallbackResult<ac::InstancePtr> result) {
                     if (result.has_error()) {
                         std::cout << "instance create error: " << result.error().text << "\n";
                         return;
@@ -87,7 +87,7 @@ int main() {
                     auto inst = result.value();
 
                     inst->runOp("run", {}, {
-                        [](ac::CallbackResult<void> result) {
+                        [model, inst](ac::CallbackResult<void> result) {
                             if (result.has_error()) {
                                 std::cout << "run error: " << result.error().text << "\n";
                                 return;
