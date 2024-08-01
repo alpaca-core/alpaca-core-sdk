cea14bb7a5616cc078424cc8a2cf1045b1a36f56
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-08-01 12:07:00 +0300

    api: remove useless move

--- api/code/ac/Dict.hpp
+++ api/code/ac/Dict.hpp
@@ -12,7 +12,7 @@ template <typename T>
 T Dict_optValueAt(Dict& dict, std::string_view key, T defaultValue) {
     auto f = dict.find(key);
     if (f == dict.end()) return defaultValue;
-    return std::move(f->get<T>());
+    return f->get<T>();
 }
 
 }
