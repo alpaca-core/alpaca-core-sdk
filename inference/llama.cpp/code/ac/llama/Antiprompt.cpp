022defbf008170d979452675f63811092058aaaf
Plamen Minev <pacominev@gmail.com>
2024-08-19 17:18:41 +0300

    llama, enhance antiprompt object to make partial checks, ref #12

--- inference/llama.cpp/code/ac/llama/Antiprompt.cpp
+++ inference/llama.cpp/code/ac/llama/Antiprompt.cpp
@@ -5,41 +5,78 @@
 
 namespace ac::llama {
 
-Antiprompt::Antiprompt()
-{}
-
 Antiprompt::Antiprompt(std::vector<std::string> antiprompts)
-    : m_antiprompts(std::move(antiprompts))
-{}
+    : m_maxAntipromptLength(0)
+    , m_antiprompts(std::move(antiprompts))
+{
+    m_antipromptState.resize(m_antiprompts.size());
+    for (uint32_t i = 0; i < m_antiprompts.size(); i++) {
+        if (m_maxAntipromptLength < m_antiprompts[i].length()) {
+            m_maxAntipromptLength = m_antiprompts[i].length();
+        }
+    }
+}
 
 void Antiprompt::addAntiPromptStr(std::string_view antiprompt) {
     m_antiprompts.push_back(std::string(antiprompt));
+    m_antipromptState.push_back({});
+
+    if (m_maxAntipromptLength < antiprompt.length()) {
+        m_maxAntipromptLength = antiprompt.length();
+    }
 }
 
 void Antiprompt::addTokenStr(std::string_view tokenStr) {
-    if (m_tokensSizes.size() == m_maxPrevTokens) {
-        m_prevTokensStr.erase(m_prevTokensStr.begin(), m_prevTokensStr.begin() + m_tokensSizes.front());
-        m_tokensSizes.pop();
-    }
-
     m_prevTokensStr += tokenStr;
-    m_tokensSizes.push(tokenStr.size());
+
+    if (m_prevTokensStr.length() > m_maxAntipromptLength) {
+        auto lengthDiff = m_prevTokensStr.length() - m_maxAntipromptLength;
+        m_prevTokensStr.erase(m_prevTokensStr.begin(), m_prevTokensStr.begin() + lengthDiff);
+
+        for (uint32_t i = 0; i < m_antipromptState.size(); i++) {
+            m_antipromptState[i].promptPos = std::max(int32_t(m_antipromptState[i].promptPos - lengthDiff), 0);
+        }
+    }
 }
 
-
 bool Antiprompt::shouldStop() {
-    for (uint32_t i = 0; i < m_antiprompts.size(); i++)
-    {
+    for (uint32_t i = 0; i < m_antiprompts.size(); i++) {
         // set the start position to search in the end of the sentence,
         // where it's possible to be located the antiprompt.
-        // We cannot search only the last token because the anti prompt might be a couple of words
-        const auto startPos = m_prevTokensStr.length() - m_antiprompts[i].length();
-        if (m_prevTokensStr.find(m_antiprompts[i].c_str(), startPos, m_antiprompts[i].length()) != std::string::npos) {
+        // We cannot search only the last token because the antiprompt might be a couple of words
+        // if (m_prevTokensStr.find(m_antiprompts[i].c_str(), startPos, m_antiprompts[i].length()) != std::string::npos) {
+        //     return true;
+        // }
+        auto& state = m_antipromptState[i];
+        uint32_t promptPos = state.promptPos;
+        if (promptPos == 0) {
+            promptPos = m_prevTokensStr.length() - m_antiprompts[i].length();
+        }
+
+        uint32_t apromptPos = state.antipromptPos;
+        for (; promptPos < m_prevTokensStr.length() && apromptPos < m_antiprompts[i].length(); promptPos++, apromptPos++) {
+            if (m_antiprompts[i][apromptPos] != m_prevTokensStr[promptPos]) {
+                state.antipromptPos = 0;
+                state.promptPos = 0;
+                break;
+            }
+        }
+
+        if (apromptPos == m_antiprompts[i].length()) {
+            state.antipromptPos = 0;
+            state.promptPos = 0;
             return true;
         }
     }
 
     return false;
+}
+
+void Antiprompt::reset() {
+    m_prevTokensStr.clear();
+    m_antiprompts.clear();
+    m_antipromptState.clear();
+    m_maxAntipromptLength = 0;
 }
 
 } // namespace ac::llama
--- inference/llama.cpp/code/ac/llama/Antiprompt.hpp
+++ inference/llama.cpp/code/ac/llama/Antiprompt.hpp
@@ -11,19 +11,28 @@
 namespace ac::llama {
 class AC_LLAMA_EXPORT Antiprompt {
 public:
-    Antiprompt();
-    Antiprompt(std::vector<std::string> antiprompts);
+    explicit Antiprompt(std::vector<std::string> antiprompts);
 
+    // adds new antiprompt to check
     void addAntiPromptStr(std::string_view antiPrompt);
 
+    // adds the new string to the checked string
     void addTokenStr(std::string_view tokenStr);
 
+    // checks if the antiprompts can be found in the added tokens so far
     bool shouldStop();
+
+    // reset the whole state of object
+    void reset();
 private:
-    const uint32_t m_maxPrevTokens = 32;
+    uint32_t m_maxAntipromptLength;
     std::string m_prevTokensStr;
-    std::queue<uint16_t> m_tokensSizes;
 
     std::vector<std::string> m_antiprompts;
+    struct State {
+        uint32_t promptPos = 0;
+        uint32_t antipromptPos = 0;
+    };
+    std::vector<State> m_antipromptState;
 };
 } // namespace ac::llama
