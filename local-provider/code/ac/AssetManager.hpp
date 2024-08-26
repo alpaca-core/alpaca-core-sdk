222fe201960f9b1adcb90f10987d93fc23b1c98a
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-08-26 17:12:02 +0300

    local: begin bare-bones asset management, ref #29

---
+++ local-provider/code/ac/AssetManager.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include "export.h"
+
+namespace ac {
+
+class AssetManagerLayer;
+
+class AC_LOCAL_EXPORT AssetManager {
+public:
+};
+
+}
