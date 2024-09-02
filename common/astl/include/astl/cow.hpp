d43ffa619e0409344f9bece5d84fcdc21679860d
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-09-02 13:18:11 +0300

    astl: finalize cow + tests

--- common/astl/include/astl/cow.hpp
+++ common/astl/include/astl/cow.hpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 //
 #pragma once
+#include <memory>
 
 // copy on write helpers
 
@@ -21,16 +22,21 @@ public:
         : m_data(std::make_shared<T>(std::forward<Args>(args)...))
     {}
 
-    sp_cow(const sp_cow& other) = default;
-    sp_cow& operator=(const sp_cow& other) = default;
+    sp_cow(const sp_cow& other) = delete;
+    sp_cow& operator=(const sp_cow& other) = delete;
     sp_cow(sp_cow&& other) noexcept = default;
     sp_cow& operator=(sp_cow&& other) noexcept = default;
 
     const T& operator*() const { return *m_data; }
     const T* operator->() const { return m_data.get(); }
 
+    std::shared_ptr<const T> detach() const noexcept { return m_data; }
+
     // explicit write
     T& w() {
+        // this is the single-producer-critical part
+        // when the producer is single it can safely check the use count as it can be sure that no other thread
+        // will concurrently increase it to 2
         if (m_data.use_count() > 1) {
             m_data = std::make_shared<T>(*m_data);
         }
