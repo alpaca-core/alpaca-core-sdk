688b0c1a73ab02c21a66732073bf0e899e9078f3
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-09-06 18:29:33 +0300

    ahttp: proper state management, ref #62

--- common/ahttp/code/ahttp/ahttp.cpp
+++ common/ahttp/code/ahttp/ahttp.cpp
@@ -11,6 +11,7 @@
 #include <furi/furi.hpp>
 
 #include <astl/throw_ex.hpp>
+#include <astl/move.hpp>
 #include <astl/iile.h>
 
 #include <limits>
@@ -34,13 +35,33 @@ bool supports_url(std::string_view url) noexcept {
 }
 
 namespace {
-itlib::generator<chunk> get_sync_coro(beast::tcp_stream& stream, beast::flat_buffer& buf, http::response_parser<http::buffer_body>& parser, size_t chunk_size) {
+template <typename Stream>
+struct sync_state {
+    std::shared_ptr<net::io_context> ctx;
+    Stream stream;
+    beast::flat_buffer buf;
+    http::response_parser<http::buffer_body> parser;
+    const size_t chunk_size;
+
+    sync_state(std::shared_ptr<net::io_context> ctx, Stream stream, size_t chunk_size)
+        : ctx(astl::move(ctx))
+        , stream(astl::move(stream))
+        , chunk_size(chunk_size)
+    {
+        parser.body_limit(std::numeric_limits<std::uint64_t>::max());
+    }
+};
+
+template <typename Stream>
+itlib::generator<chunk> get_sync_coro(std::unique_ptr<sync_state<Stream>> state) {
     const auto total_size = iile([&]() -> std::optional<size_t> {
-        auto ret = parser.content_length();
+        // silly convert boost::optional to std::optional
+        auto ret = state->parser.content_length();
         if (ret) return *ret;
         return {};
     });
 
+    auto chunk_size = state->chunk_size;
     if (total_size) {
         if (chunk_size > *total_size) {
             chunk_size = *total_size;
@@ -56,13 +77,13 @@ itlib::generator<chunk> get_sync_coro(beast::tcp_stream& stream, beast::flat_buf
     size_t offset = 0;
     co_yield chunk{total_size, offset, chunk_buf};
 
-    auto& body = parser.get().body();
-    while (!parser.is_done()) {
+    auto& body = state->parser.get().body();
+    while (!state->parser.is_done()) {
         chunk_buf.resize(chunk_size);
         body.data = chunk_buf.data();
         body.size = chunk_buf.size();
         beast::error_code ec;
-        http::read(stream, buf, parser, ec);
+        http::read(state->stream, state->buf, state->parser, ec);
 
         if (ec && ec != http::error::need_buffer) {
             throw boost::system::system_error{ ec };
@@ -74,31 +95,29 @@ itlib::generator<chunk> get_sync_coro(beast::tcp_stream& stream, beast::flat_buf
     }
 }
 
-itlib::generator<chunk> get_sync(net::io_context& ctx, std::string_view url, size_t chunk_size);
+itlib::generator<chunk> get_sync(std::shared_ptr<net::io_context> ctx, std::string_view url, size_t chunk_size);
 
 template <typename Stream>
-itlib::generator<chunk> get_sync_t(net::io_context& ctx, Stream& stream, const http::request<http::empty_body>& req, size_t chunk_size) {
-    http::write(stream, req);
+itlib::generator<chunk> get_sync_t(std::unique_ptr<sync_state<Stream>> state, const http::request<http::empty_body>& req) {
+    http::write(state->stream, req);
 
-    beast::flat_buffer buf;
-    http::response_parser<http::buffer_body> parser;
-    parser.body_limit(std::numeric_limits<std::uint64_t>::max());
+    state->parser.body_limit(std::numeric_limits<std::uint64_t>::max());
 
-    http::read_header(stream, buf, parser);
-    auto& header = parser.get().base();
+    http::read_header(state->stream, state->buf, state->parser);
+    auto& header = state->parser.get().base();
     std::cout << header << std::endl;
 
     if (auto f = header.find(http::field::location); f != header.end()) {
         // redirect... so recurse
         auto location = f->value();
         std::cout << "redirecting to: " << location << std::endl;
-        return get_sync(ctx, location, chunk_size);
+        return get_sync(state->ctx, location, state->chunk_size);
     }
 
-    return get_sync_coro(stream, buf, parser, chunk_size);
+    return get_sync_coro(astl::move(state));
 }
 
-itlib::generator<chunk> get_sync(net::io_context& ctx, std::string_view url, size_t chunk_size) {
+itlib::generator<chunk> get_sync(std::shared_ptr<net::io_context> ctx, std::string_view url, size_t chunk_size) {
     auto splitUrl = furi::uri_split::from_uri(url);
 
     http::request<http::empty_body> req;
@@ -112,16 +131,17 @@ itlib::generator<chunk> get_sync(net::io_context& ctx, std::string_view url, siz
     //req.set(http::field::range, "1-5");
     //std::cout << req << std::endl;
 
-    beast::tcp_stream stream(ctx);
+    beast::tcp_stream stream(*ctx);
 
     auto resolved = [&]() {
-        net::ip::tcp::resolver resolver(ctx);
+        net::ip::tcp::resolver resolver(*ctx);
         return resolver.resolve(splitUrl.authority, splitUrl.scheme);
     }();
     stream.connect(resolved);
 
     if (splitUrl.scheme == "http") {
-        return get_sync_t(ctx, stream, req, chunk_size);
+        auto state = std::make_unique<sync_state<beast::tcp_stream>>(ctx, astl::move(stream), chunk_size);
+        return get_sync_t(astl::move(state), req);
     }
 
     ac::throw_ex{} << "unsupported scheme: " << splitUrl.scheme;
@@ -132,9 +152,8 @@ itlib::generator<chunk> get_sync(net::io_context& ctx, std::string_view url, siz
 }
 }
 
-itlib::generator<chunk> get_sync(std::string_view uri, size_t chunk_size) {
-    net::io_context ctx;
-    return get_sync(ctx, uri, chunk_size);
+itlib::generator<chunk> get_sync(std::string_view url, size_t chunk_size) {
+    return get_sync(std::make_shared<net::io_context>(), url, chunk_size);
 }
 
 } // namespace dl
--- common/ahttp/test/t-ahttp-sync.cpp
+++ common/ahttp/test/t-ahttp-sync.cpp
@@ -10,3 +10,12 @@ TEST_CASE("supports_url") {
     CHECK_FALSE(ahttp::supports_url("git://example.com"));
     CHECK_FALSE(ahttp::supports_url("asdf"));
 }
+
+TEST_CASE("just get") {
+    auto gen = ahttp::get_sync("http://httpbin.org/bytes/128?seed=42");
+    auto init = gen.next();
+    REQUIRE(init);
+    CHECK(init->size.value_or(0) == 128);
+    CHECK(init->offset == 0);
+    CHECK(init->data.size() == 0);
+}
