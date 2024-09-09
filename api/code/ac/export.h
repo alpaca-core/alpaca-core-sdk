17f5916b0275a0342bcc8729c397b50891f6b1da
Tzanko Matev <tzanko.m@santiment.net>
2024-09-09 13:30:47 +0300

    docs: Remove export.h from processed files #44

--- api/code/ac/export.h
+++ api/code/ac/export.h
@@ -4,33 +4,12 @@
 #pragma once
 #include <splat/symbol_export.h>
 
-/**
- * @file export.h
- * @brief Defines macros for exporting and importing symbols in the AC API.
- *
- * This file provides macros for marking symbols as exported or imported, which is
- * necessary for building shared libraries on various platforms. It uses the splat
- * library's symbol_export.h for platform-specific implementations.
- */
-
 #if AC_API_SHARED
 #   if BUILDING_AC_API
-        /**
-         * @brief Macro for exporting symbols when building the AC API as a shared library.
-         */
 #       define AC_API_EXPORT SYMBOL_EXPORT
 #   else
-        /**
-         * @brief Macro for importing symbols when using the AC API as a shared library.
-         */
 #       define AC_API_EXPORT SYMBOL_IMPORT
 #   endif
 #else
-    /**
-     * @brief Macro for symbols when the AC API is not built as a shared library.
-     *
-     * This macro is defined as empty when the API is not built as a shared library,
-     * effectively making the symbols neither exported nor imported.
-     */
 #   define AC_API_EXPORT
 #endif
