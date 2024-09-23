cb711eb229085b1105e7853fa35cc7bcf38198c9
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-09-23 10:52:26 +0300

    java: dict conversion use jni::Box, ref #114, #115

--- wrapper/java/code/JniDict.cpp
+++ wrapper/java/code/JniDict.cpp
@@ -44,27 +44,6 @@ struct HashMapClass {
     }
 };
 
-struct BooleanTag {
-    static constexpr auto Name() { return "java/lang/Boolean"; }
-    using cpp_t = jboolean;
-    static constexpr auto valueFuncName = "booleanValue";
-};
-struct IntegerTag {
-    static constexpr auto Name() { return "java/lang/Integer"; }
-    using cpp_t = jint;
-    static constexpr auto valueFuncName = "intValue";
-};
-struct LongTag {
-    static constexpr auto Name() { return "java/lang/Long"; }
-    using cpp_t = jlong;
-    static constexpr auto valueFuncName = "longValue";
-};
-struct DoubleTag {
-    static constexpr auto Name() { return "java/lang/Double"; }
-    using cpp_t = jdouble;
-    static constexpr auto valueFuncName = "doubleValue";
-};
-
 template <class OutTagType, class T>
 jni::Local<jni::Object<OutTagType>> unsafeCast(jni::JNIEnv& env, const T& object)
 {
@@ -72,91 +51,33 @@ jni::Local<jni::Object<OutTagType>> unsafeCast(jni::JNIEnv& env, const T& object
     return jni::Local<jni::Object<OutTagType>>(env, ptr);
 }
 
-template <typename Tag>
-struct PrimitiveClass {
-    jni::JNIEnv& env;
-
-    using cpp_t = typename Tag::cpp_t;
-
-    jni::Local<jni::Class<Tag>> cls;
-    jni::Constructor<Tag, cpp_t> ctor;
-    jni::Method<Tag, cpp_t()> valueFunc;
-
-    PrimitiveClass(jni::JNIEnv& env)
-        : env(env),
-        cls(jni::Class<Tag>::Find(env)),
-        ctor(cls.template GetConstructor<cpp_t>(env)),
-        valueFunc(cls.template GetMethod<cpp_t()>(env, Tag::valueFuncName))
-    {}
-
-    struct Instance {
-        const PrimitiveClass& parent;
-        jni::Local<jni::Object<Tag>> obj;
-
-        cpp_t value() const {
-            return obj.Call(parent.env, parent.valueFunc);
-        }
-    };
-
-    Instance newInstance(cpp_t value) const {
-        return {*this, cls.New(env, ctor, value)};
-    }
-
-    Instance cast(jni::Local<Obj> obj) const {
-        return {
-            *this,
-            unsafeCast<Tag>(env, std::move(obj))
-        };
-    }
-
-    std::optional<cpp_t> safeGet(jni::Local<Obj>& obj) const {
-        if (!obj.IsInstanceOf(env, cls)) {
-            return {};
-        }
-
-        return cast(std::move(obj)).value();
-    }
-};
-
-using BooleanClass = PrimitiveClass<BooleanTag>;
-using IntegerClass = PrimitiveClass<IntegerTag>;
-using LongClass = PrimitiveClass<LongTag>;
-using DoubleClass = PrimitiveClass<DoubleTag>;
 
 struct DictToMapConverter {
     jni::JNIEnv& env;
 
     HashMapClass hashMapCls;
-    BooleanClass boolCls;
-    IntegerClass intCls;
-    LongClass longCls;
-    DoubleClass doubleCls;
 
     DictToMapConverter(jni::JNIEnv& env)
         : env(env)
         , hashMapCls(env)
-        , boolCls(env)
-        , intCls(env)
-        , longCls(env)
-        , doubleCls(env)
     {}
 
     jni::Local<Obj> convert(const Dict& dict) {
         switch (dict.type()) {
         case Dict::value_t::boolean:
-            return boolCls.newInstance(dict.get<bool>()).obj;
+            return jni::Box(env, jboolean(dict.get<bool>()));
         case Dict::value_t::number_integer:
-            return intCls.newInstance(dict.get<int>()).obj;
+            return jni::Box(env, dict.get<jint>());
         case Dict::value_t::number_unsigned: {
             auto uval = dict.get<unsigned>();
             if (uval <= unsigned(std::numeric_limits<jint>::max())) {
                 // return int if it fits
-                return intCls.newInstance(uval).obj;
+                return jni::Box(env, jint(uval));
             }
-            return longCls.newInstance(uval).obj;
+            return jni::Box(env, jlong(uval));
         }
         case Dict::value_t::number_float:
-            return doubleCls.newInstance(dict.get<double>()).obj;
+            return jni::Box(env, dict.get<jdouble>());
         case Dict::value_t::string: {
             auto sv = dict.get<std::string_view>();
             auto begin = sv.data();
@@ -279,10 +200,6 @@ struct MapToDictConverter {
     jni::Local<jni::Class<jni::ArrayTag<jbyte>>> byteArrClass;
 
     MapClass mapCls;
-    BooleanClass boolCls;
-    IntegerClass intCls;
-    LongClass longCls;
-    DoubleClass doubleCls;
 
     MapToDictConverter(jni::JNIEnv& env)
         : env(env)
@@ -290,10 +207,6 @@ struct MapToDictConverter {
         , objArrayClass(jni::Class<jni::ArrayTag<Obj>>::Find(env))
         , byteArrClass(jni::Class<jni::ArrayTag<jbyte>>::Find(env))
         , mapCls(env)
-        , boolCls(env)
-        , intCls(env)
-        , longCls(env)
-        , doubleCls(env)
     {}
 
     bool getNull(const jni::Local<Obj>& obj) {
@@ -370,6 +283,23 @@ struct MapToDictConverter {
         return ret;
     }
 
+    template <typename Tag, typename CppType>
+    struct SafeUnboxer {
+        static std::optional<CppType> safeGet(jni::JNIEnv& env, const jni::Local<Obj>& obj) {
+            if (!obj.IsInstanceOf(env, jni::Class<Tag>::Singleton(env))) {
+                return {};
+            }
+
+            auto cast = unsafeCast<Tag>(env, obj);
+            return jni::Unbox(env, cast);
+        }
+    };
+
+    using BoolClass = SafeUnboxer<jni::BooleanTag, jni::jboolean>;
+    using IntClass = SafeUnboxer<jni::IntegerTag, jni::jint>;
+    using LongClass = SafeUnboxer<jni::LongTag, jni::jlong>;
+    using DoubleClass = SafeUnboxer<jni::DoubleTag, jni::jdouble>;
+
     Dict convert(jni::Local<Obj> obj) {
         if (getNull(obj)) {
             return {};
@@ -377,13 +307,13 @@ struct MapToDictConverter {
         if (auto str = safeGetString(obj)) {
             return std::move(*str);
         }
-        if (auto b = boolCls.safeGet(obj)) {
+        if (auto b = BoolClass::safeGet(env, obj)) {
             return !!*b;
         }
-        if (auto i = intCls.safeGet(obj)) {
+        if (auto i = IntClass::safeGet(env, obj)) {
             return *i;
         }
-        if (auto l = longCls.safeGet(obj)) {
+        if (auto l = LongClass::safeGet(env, obj)) {
             auto val = *l;
             // longs are special...
 
@@ -406,7 +336,7 @@ struct MapToDictConverter {
 
             throw std::runtime_error("long value too large");
         }
-        if (auto d = doubleCls.safeGet(obj)) {
+        if (auto d = DoubleClass::safeGet(env, obj)) {
             return *d;
         }
         if (auto arr = safeGetArray(obj)) {
