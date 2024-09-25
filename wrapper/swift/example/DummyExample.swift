23551c9abcbccc818b4d8a98a19764e55f77493e
Plamen Minev <pacominev@gmail.com>
2024-09-25 19:07:55 +0300

    Flx wrapper of to be buildable and runnable properly
    -- the compiler option for interoperability is crucial to be set in all swift projects

---
+++ wrapper/swift/example/DummyExample.swift
@@ -0,0 +1,13 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+import Foundation
+import AlpacaCoreSW
+
+@main
+struct ACExample {
+    static func main() {
+        AlpacaCoreSW.test()
+        print("sdasdasda")
+    }
+}
