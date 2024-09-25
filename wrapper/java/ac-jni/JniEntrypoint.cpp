322e5e3d51bbde4cf14cac912a16e4a861b569a4
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-09-25 15:17:23 +0300

    java: enable logging in jni module, ref #42

--- wrapper/java/ac-jni/JniEntrypoint.cpp
+++ wrapper/java/ac-jni/JniEntrypoint.cpp
@@ -1,9 +1,16 @@
 #include "jni.hpp"
 #include "JniSandbox.hpp"
 #include "JniApi.hpp"
+
+#include <jalog/Instance.hpp>
+#include <jalog/sinks/DefaultSink.hpp>
+
 #include <iostream>
 
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* jvm, void*) {
+    jalog::Instance jl;
+    jl.setup().add<jalog::sinks::DefaultSink>();
+
     auto& env = jni::GetEnv(*jvm);
 
     ac::java::JniSandbox_register(env);
