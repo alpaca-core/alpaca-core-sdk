9ef34a05cfe4dd19bf1677abc58e2e1ec6190faa
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-09-27 09:44:39 +0300

    c: simplify local structure, add llama example, ref #121

--- wrapper/c/local/code/ac/local_dummy.cpp
+++ wrapper/c/local/code/ac/local_dummy.cpp
@@ -2,11 +2,9 @@
 // SPDX-License-Identifier: MIT
 //
 #include "local_dummy.h"
-#include <ac/local.h>
+#include "local.h"
 #include <ac/local/LocalDummy.hpp>
 
-extern "C" {
-void ac_add_local_dummy_inference(struct ac_local_model_factory* factory) {
+extern "C" void ac_add_local_dummy_inference(struct ac_local_model_factory* factory) {
     ac::local::addDummyInference(ac::local::cutil::ModelFactory_toCpp(factory));
-}
 }
