617dd661744a032f16b74713f81131dcb1dafc20
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-09-27 17:24:59 +0300

    java: complete tests, closes #121

--- wrapper/java/src/testFixtures/java/com/alpacacore/TestLocalDummy.java
+++ wrapper/java/src/testFixtures/java/com/alpacacore/TestLocalDummy.java
@@ -45,5 +45,57 @@ public class TestLocalDummy {
         assertNotNull(model);
         assertEquals("synthetic", pdata.tag);
         assertEquals(0.5, pdata.progress, 0.0001);
+
+        try {
+            Instance instance = model.createInstance("nope", null);
+            fail("Should have thrown an exception");
+        }
+        catch (Error e) {
+            assertEquals("dummy: unknown instance type: nope", e.getMessage());
+        }
+
+        try {
+            Instance instance = model.createInstance("general", Map.of("cutoff", 100));
+            fail("Should have thrown an exception");
+        }
+        catch (Error e) {
+            assertEquals("Cutoff 100 greater than model size 22", e.getMessage());
+        }
+
+        Instance instance = model.createInstance("general", null);
+        assertNotNull(instance);
+
+        try {
+            instance.runOp("nope", null, null);
+            fail("Should have thrown an exception");
+        }
+        catch (Error e) {
+            assertEquals("dummy: unknown op: nope", e.getMessage());
+        }
+
+        Map result = (Map)instance.runOp("run", Map.of("input", new String[]{"a", "b"}), null);
+        assertNotNull(result);
+        String opResult = (String)result.get("result");
+        assertEquals("a one b two", opResult);
+
+        try {
+            instance.runOp("run", Map.of("input", new String[]{"a", "b", "c"}, "throw_on", 3), null);
+            fail("Should have thrown an exception");
+        }
+        catch (Error e) {
+            assertEquals("Throw on token 3", e.getMessage());
+        }
+
+        Instance cutoffInstance = model.createInstance("general", Map.of("cutoff", 2));
+        assertNotNull(cutoffInstance);
+
+        result = (Map)cutoffInstance.runOp("run", Map.of("input", new String[]{"a", "b", "c"}), null);
+        assertNotNull(result);
+        opResult = (String)result.get("result");
+        assertEquals("a one b two c one", opResult);
+
+        AlpacaCore.releaseInstance(cutoffInstance);
+        AlpacaCore.releaseInstance(instance);
+        AlpacaCore.releaseModel(model);
     }
 }
