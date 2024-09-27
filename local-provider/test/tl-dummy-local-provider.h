d973764cf731ad1350f57bf86f340daf60e2d588
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-09-27 09:47:30 +0300

    remove obsolete subdirs, ref #121

--- local-provider/test/tl-dummy-local-provider.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Alpaca Core
-// SPDX-License-Identifier: MIT
-//
-#include <ac/local_provider.h>
-#include <stdint.h>
-
-uint64_t get_thread_id(void);
-void add_dummy_inference(ac_local_provider* local_provider);
