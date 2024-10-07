37228d9a84d57fc18cdb71cd3a0d1857d0a733b5
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-10-07 16:42:24 +0300

    docs(dict): add dict to docs and cleanup comments, ref #44

--- dict/code/ac/Dict.hpp
+++ dict/code/ac/Dict.hpp
@@ -6,28 +6,13 @@
 
 namespace ac {
 
-/**
- * @brief A type alias for nlohmann::json, representing a dictionary-like structure.
- */
+/// Dictionary type. A type alias for [nlohmann::json](https://github.com/nlohmann/json).
 using Dict = nlohmann::json;
 
-/**
- * @brief A type alias for a vector of bytes, representing binary data.
- */
+/// Blob (binary) carrier type.
 using Blob = std::vector<uint8_t>;
 
-/**
- * @brief Retrieves an optional value from a Dict object.
- *
- * This function attempts to find a value in the Dict object using the provided key.
- * If the key is not found, it returns the default value.
- *
- * @tparam T The type of the value to retrieve and return.
- * @param dict The Dict object to search in.
- * @param key The key to look for in the Dict.
- * @param defaultValue The value to return if the key is not found.
- * @return The value associated with the key if found, otherwise the default value.
- */
+/// Utility function to return an optional value from a Dict object at a given key.
 template <typename T>
 T Dict_optValueAt(const Dict& dict, std::string_view key, T defaultValue) {
     auto f = dict.find(key);
@@ -35,19 +20,9 @@ T Dict_optValueAt(const Dict& dict, std::string_view key, T defaultValue) {
     return f->get<T>();
 }
 
-/**
- * @brief Applies a value from a Dict object to a given variable if the key exists.
- *
- * This function attempts to find a value in the Dict object using the provided key.
- * If the key is found, it applies the value to the provided variable.
- * If the key is not found, the variable remains unchanged.
- *
- * @tparam T The type of the value to retrieve and apply.
- * @param dict The Dict object to search in.
- * @param key The key to look for in the Dict.
- * @param value The variable to which the value will be applied if found.
- * @return true if the key was found and the value was applied, otherwise false.
- */
+/// Utility function to get a value from a Dict object at a given key.
+/// If the key is found, it sets the value to the output parameter and returns true.
+/// If the key is not found, it returns false.
 template <typename T>
 bool Dict_optApplyValueAt(const Dict& dict, std::string_view key, T& value) {
     auto f = dict.find(key);
