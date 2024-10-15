88b194e37bab6875a7befbf3371b2a22f16f386d
Plamen Minev <pacominev@gmail.com>
2024-10-15 17:40:17 +0300

    feat: add tests for swift api, ref #120

---
+++ wrapper/swift/test/t-SwiftApi.swift
@@ -0,0 +1,35 @@
+import AlpacaCoreSwift
+import XCTest
+
+final class SwiftApi: XCTestCase {
+    func testModelCreation() throws {
+        print("Hello from e-Dummy.swift")
+
+        AlpacaCoreSwift.initSDK();
+
+        var desc = AlpacaCoreSwift.ModelDesc()
+        desc.inferenceType = "dummy"
+        desc.name = "synthetic dummy"
+
+        let dict = Dictionary<String, Any>()
+        let model = AlpacaCoreSwift.createModel(&desc, dict, {
+            (progress: Float) in
+            print("Progress: \(progress)")
+        });
+        XCTAssertNotNil(model)
+
+        let instance = model!.createInstance("general", dict);
+        XCTAssertNotNil(instance)
+
+        let params = [
+            "input": ["a", "b"]
+        ]
+
+        let result = instance.runOp("run", params, {
+            (progress: Float) in
+            print("Progress: \(progress)")
+        });
+
+        XCTAssertEqual(result["result"] as! String, "a one b two", "Incorrect result!")
+    }
+}
