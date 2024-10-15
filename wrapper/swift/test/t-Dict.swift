752163537a8c618b2908f3e5e5d2c5697996ddd7
Plamen Minev <pacominev@gmail.com>
2024-10-15 16:19:47 +0300

    feat: add infrastructure for swift tests runnable from cmake, ref #119

--- wrapper/swift/test/t-Dict.swift
+++ wrapper/swift/test/t-Dict.swift
@@ -1,8 +1,28 @@
-import Testing
-@testable import AlpacaCoreSwift
+import AlpacaCoreSwift
+import XCTest
 
-@main struct Runner {
-    static func main() async {
-        await Testing.__swiftPMEntryPoint() as Never
+final class DictTests: XCTestCase {
+    func testDictionaryToDict() throws {
+        let pesho = String("john.appleseed@apple.com")
+        XCTAssertEqual(pesho, "john.appleseed@apple.com", "sdads")
+
+        let dictionary: [String: Any] = [
+            "name": "Alice",
+            "age": 28,
+            "height": 1.6,
+            "isMember": true,
+            "scores": [88, 92, 79, "100"],
+            "address": [
+                "city": "Wonderland",
+                "postalCode": "12345"
+            ]
+        ]
+
+        let translated = AlpacaCoreSwift.translateDictionaryToDict(dictionary)
+        let translatedRef = translated.getRef()
+        XCTAssertEqual(String(translatedRef.atKey("name").getString()), dictionary["name"] as! String, "Couldn't convert String properly!")
+        XCTAssertEqual(translatedRef.atKey("age").getInt(), dictionary["age"] as! Int, "Couldn't convert Int properly!")
+        XCTAssertEqual(translatedRef.atKey("height").getDouble(), dictionary["height"] as! Double, "Couldn't convert Double properly!")
+        XCTAssertEqual(translatedRef.atKey("isMember").getBool(), dictionary["isMember"] as! Bool, "Couldn't convert Bool properly!")
     }
 }
