7d2a82cbfac005f0f42230b6fe54d93dcdc3ab13
Plamen Minev <pacominev@gmail.com>
2024-10-21 13:53:14 +0300

    enhance: improve dict converter in objC++

--- wrapper/cocoa/code/ac/DictConverter.mm
+++ wrapper/cocoa/code/ac/DictConverter.mm
@@ -2,72 +2,67 @@
 // SPDX-License-Identifier: MIT
 //
 #import "DictConverter.hpp"
+#include <string_view>
 
 #include <ac/Dict.hpp>
 
 @implementation DictConverter
+
+// Helper function for determine the type of the value
++ (ac::Dict)convertDictValue:(id)value {
+    if ([value isKindOfClass:[NSDictionary class]]) {
+        return [self convertToACDict:(NSDictionary *)value];
+    }
+    else if ([value isKindOfClass:[NSArray class]]) {
+        ac::Dict arrayJson = ac::Dict::array();
+        for (id item in (NSArray *)value) {
+            arrayJson.push_back([self convertDictValue:item]);
+        }
+        return arrayJson;
+    }
+    else if([value isKindOfClass:[NSString class]]){
+        return  std::string([(NSString *)value UTF8String]);
+    }
+    else if ([value isKindOfClass:[NSNumber class]]) {
+        // Get the type of the NSNumber
+        std::string_view typeStr = [value objCType];
+
+        if (typeStr == @encode(BOOL) || typeStr == @encode(char)) {
+            return [value boolValue];
+        }
+        // Handle integer values
+        else if (typeStr == @encode(int) || typeStr == @encode(NSInteger) ||
+                typeStr == @encode(long) || typeStr == @encode(long long)) {
+            return [value integerValue]; // Handle integer numbers
+        }
+        else if(typeStr == @encode(unsigned int) || typeStr == @encode(NSUInteger) ||
+                typeStr == @encode(unsigned long) || typeStr == @encode(unsigned long long)) {
+            return [value unsignedIntegerValue]; // Handle unsigned integer numbers
+        }
+        // Handle floating-point values
+        else if (typeStr == @encode(float) || typeStr == @encode(double)) {
+            return [value doubleValue]; // Handle floating point numbers
+        }
+    }
+    else if ([value isKindOfClass:[NSData class]]) {
+        const uint8_t* bytes = (const uint8_t*)[value bytes];
+        return ac::Dict::binary(ac::Blob(bytes, bytes + [value length]));
+    }
+
+    NSException *exception = [NSException exceptionWithName: @"DictConverterException"
+                                                    reason: @"Invalid type for dictionary value"
+                                                    userInfo: nil];
+    @throw exception;
+
+    return ac::Dict();
+}
 
 + (ac::Dict)convertToACDict:(NSDictionary<NSString *, id> *)dictionary {
     ac::Dict jsonObj;
 
     for (NSString *key in dictionary) {
         id value = dictionary[key];
-
-        if ([value isKindOfClass:[NSDictionary class]]) {
-            jsonObj[key.UTF8String] = [self convertToACDict:(NSDictionary *)value];
-        }
-
-        if ([value isKindOfClass:[NSArray class]]) {
-            ac::Dict arrayJson = ac::Dict::array();
-            for (id item in (NSArray *)value) {
-                if ([item isKindOfClass:[NSDictionary class]]) {
-                    arrayJson.push_back([self convertToACDict:(NSDictionary *)item]);
-                } else if ([item isKindOfClass:[NSString class]]) {
-                    arrayJson.push_back(std::string([(NSString *)item UTF8String]));
-                } else if ([item isKindOfClass:[NSNumber class]]) {
-                    // Get the type of the NSNumber
-                    const char *type = [item objCType];
-                    // Check if the integerValue and doubleValue are the same
-                    if (strcmp(type, @encode(BOOL)) == 0 || strcmp(type, @encode(char)) == 0) {
-                        arrayJson.push_back([item boolValue]);
-                    }
-                    // Handle integer values
-                     else if (strcmp(type, @encode(int)) == 0 || strcmp(type, @encode(NSInteger)) == 0 ||
-                              strcmp(type, @encode(long)) == 0 || strcmp(type, @encode(long long)) == 0) {
-                         arrayJson.push_back([item integerValue]); // Handle integer numbers
-                     }
-                     // Handle floating-point values
-                     else if (strcmp(type, @encode(float)) == 0 || strcmp(type, @encode(double)) == 0) {
-                         arrayJson.push_back([item doubleValue]); // Handle floating point numbers
-                     }
-                }
-            }
-            jsonObj[key.UTF8String] = arrayJson;
-        }
-        if ([value isKindOfClass:[NSString class]]) {
-            jsonObj[key.UTF8String] = std::string([(NSString *)value UTF8String]);
-        }
-        if ([value isKindOfClass:[NSNumber class]]) {
-            // Get the type of the NSNumber
-            const char *type = [value objCType];
-            // Check if the integerValue and doubleValue are the same
-            if (strcmp(type, @encode(BOOL)) == 0 || strcmp(type, @encode(char)) == 0) {
-                jsonObj[key.UTF8String] = [value boolValue]; // Handle BOOL
-            }
-            // Handle integer values
-             else if (strcmp(type, @encode(int)) == 0 || strcmp(type, @encode(NSInteger)) == 0 ||
-                      strcmp(type, @encode(long)) == 0 || strcmp(type, @encode(long long)) == 0) {
-                 jsonObj[key.UTF8String] = [value integerValue];
-             }
-             // Handle floating-point values
-             else if (strcmp(type, @encode(float)) == 0 || strcmp(type, @encode(double)) == 0) {
-                 jsonObj[key.UTF8String] = [value doubleValue];
-             }
-        }
-        if ([value isKindOfClass:[NSData class]]) {
-            const uint8_t* bytes = (const uint8_t*)[value bytes];
-            jsonObj[key.UTF8String] = ac::Dict::binary(ac::Blob(bytes, bytes + [value length]));
-        }
+        jsonObj[key.UTF8String] = [self convertDictValue:value];
     }
 
     return jsonObj;
@@ -98,6 +93,12 @@
     } else if (json.is_null()) {
         return @{};
     }
+
+    NSException *exception = [NSException exceptionWithName: @"DictConverterException"
+                                                reason: @"Invalid type for dictionary value"
+                                                userInfo: nil];
+    @throw exception;
+
     return nil;
 }
 
