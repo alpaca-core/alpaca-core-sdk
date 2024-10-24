6e67f7027624b20c6acd45a22ce676584578d364
Plamen Minev <pacominev@gmail.com>
2024-10-24 11:13:05 +0300

    fix: review comments

--- wrapper/swift/example/ac-audio-swift.hpp
+++ wrapper/swift/example/ac-audio-swift.hpp
@@ -1,10 +1,13 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
 #pragma once
 
 #include "../../../common/ac-audio/code/ac-audio.hpp"
 
 namespace AC {
 
-std::vector<float> loadWavF32Mono(const std::string& path) {
+inline std::vector<float> loadWavF32Mono(const std::string& path) {
     return ac::audio::loadWavF32Mono(path);
 }
 
