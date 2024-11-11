e96ffc71cecb1f0a7a11a9e475eaab587c6a937d
Borislav Stanimirov <b@ibob.bg>
2024-11-11 16:43:51 +0200

    build: move java wrapper to new repo, ref #184

--- wrapper/java/ac-jni/JniApi.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Alpaca Core
-// SPDX-License-Identifier: MIT
-//
-#pragma once
-#include "jni.hpp"
-
-namespace ac::java {
-void JniApi_register(jni::JavaVM& jvm, jni::JNIEnv& env);
-}
