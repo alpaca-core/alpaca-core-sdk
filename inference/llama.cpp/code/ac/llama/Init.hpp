c72485ee0767ce7ea178bda2a78ee66df6a6b74b
Borislav Stanimirov <b@ibob.bg>
2024-11-18 11:58:33 +0200

    chore: llama inference moved to alpaca-core/ilib-llama.cpp, ref #177

--- inference/llama.cpp/code/ac/llama/Init.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Alpaca Core
-// SPDX-License-Identifier: MIT
-//
-#pragma once
-#include "export.h"
-
-namespace ac::llama {
-AC_LLAMA_EXPORT void initLibrary();
-} // namespace ac::llama
--- inference/llama.cpp/code/ac/llama/Logging.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Alpaca Core
-// SPDX-License-Identifier: MIT
-//
-#include "Logging.hpp"
-
-namespace ac::llama::log {
-jalog::Scope scope("ac:llama");
-}
