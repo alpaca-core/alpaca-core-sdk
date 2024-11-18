94d2a86988657f50ea4424abb1c1980feec9168b
Borislav Stanimirov <b@ibob.bg>
2024-11-18 10:15:26 +0200

    feat(lib): expose common functionalities at lib level, ref #177

--- local/code/ac/local/Lib.cpp
+++ local/code/ac/local/Lib.cpp
@@ -16,8 +16,20 @@ ModelLoaderRegistry& Lib::modelLoaderRegistry() {
     return g_modelLoaderRegistry;
 }
 
+void Lib::addLoader(ModelLoader& loader) {
+    g_modelLoaderRegistry.addLoader(loader);
+}
+
 PluginManager& Lib::pluginManager() {
     return g_pluginManager;
+}
+
+const PluginInfo* Lib::loadPlugin(const std::string& path) {
+    return g_pluginManager.loadPlugin(path);
+}
+
+void Lib::loadAllPlugins() {
+    g_pluginManager.loadPlugins();
 }
 
 } // namespace ac::local
--- local/code/ac/local/Lib.hpp
+++ local/code/ac/local/Lib.hpp
@@ -4,14 +4,24 @@
 #pragma once
 #include "export.h"
 #include "Version.hpp"
+#include <string>
 
 namespace ac::local {
 class ModelLoaderRegistry;
+class ModelLoader;
+
 class PluginManager;
+struct PluginInfo;
 
 struct AC_LOCAL_EXPORT Lib {
     static ModelLoaderRegistry& modelLoaderRegistry();
+
+    static void addLoader(ModelLoader& loader);
+
     static PluginManager& pluginManager();
+
+    static const PluginInfo* loadPlugin(const std::string& path);
+    static void loadAllPlugins();
 };
 
 } // namespace ac::local
