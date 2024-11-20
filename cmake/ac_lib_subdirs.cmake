b8bb7e6b9a90788280ab6f7ce4a3462a9146ec05
Borislav Stanimirov <b@ibob.bg>
2024-11-20 07:55:49 +0200

    chore: cleanup cmake, ref #190

--- cmake/ac_lib_subdirs.cmake
+++ cmake/ac_lib_subdirs.cmake
@@ -4,26 +4,6 @@
 
 # macros which optionally add subdirs according to the project options
 
-macro(ac_add_tools_subdir)
-    if(AC_BUILD_TOOLS)
-        add_subdirectory(tools)
-    endif()
-endmacro()
 
-macro(ac_add_test_subdir)
-    if(AC_BUILD_TESTS)
-        add_subdirectory(test)
-    endif()
-endmacro()
 
-macro(ac_add_example_subdir)
-    if(AC_BUILD_EXAMPLES)
-        add_subdirectory(example)
-    endif()
-endmacro()
 
-macro(ac_add_local_subdir)
-    if(AC_BUILD_LOCAL)
-        add_subdirectory(local)
-    endif()
-endmacro()
