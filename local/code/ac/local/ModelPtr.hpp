1dfd84a70374426e44f3ef9465e0013d33675133
Borislav Stanimirov <b@ibob.bg>
2024-11-27 16:35:18 +0200

    docs: minor typo

--- local/code/ac/local/ModelPtr.hpp
+++ local/code/ac/local/ModelPtr.hpp
@@ -9,7 +9,7 @@ namespace ac::local {
 class Model;
 
 /// A shared pointer type for Model objects.
-/// Ideally this would a local shared pointer, but this on its own doesn't warrant including boost as a dependency.
+/// Ideally this would be a local shared pointer, but this on its own doesn't warrant including boost as a dependency.
 /// The SDK does not make use of this shared pointer, but it provides it for convenience in case the user needs it.
 /// @ingroup cpp-local
 using ModelPtr = std::shared_ptr<Model>;
