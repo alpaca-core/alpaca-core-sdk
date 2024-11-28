ed2f13a493b854b4222274f0ac770a5a8d0de475
Borislav Stanimirov <b@ibob.bg>
2024-11-28 15:19:08 +0200

    feat(astl): tuple switches

--- astl/include/astl/tuple_util.hpp
+++ astl/include/astl/tuple_util.hpp
@@ -25,7 +25,42 @@ struct type_index<Q, std::tuple<T, Ts...>, I> {
     static inline constexpr int value = type_index<Q, std::tuple<Ts...>, I + 1>::value;
 };
 
-template <typename Q, typename Tulpe>
-inline constexpr int type_index_v = type_index<Q, Tulpe>::value;
+template <typename Q, typename Tuple>
+inline constexpr int type_index_v = type_index<Q, std::remove_cv_t<Tuple>>::value;
+
+namespace impl {
+
+template <int I>
+struct switch_index {
+    template <typename Tuple, typename VFunc>
+    static constexpr decltype(auto) switch_case(Tuple& t, int i, VFunc&& f) {
+        if (i == I) {
+            return f(std::get<I>(t));
+        }
+        else {
+            return switch_index<I - 1>::switch_case(t, i, std::forward<VFunc>(f));
+        }
+    }
+};
+
+template <>
+struct switch_index<-1> {
+    template <typename Tuple, typename VFunc>
+    static constexpr auto switch_case(Tuple&, int, VFunc&& f) -> decltype(f()) {
+        return f();
+    }
+};
+
+} // namespace impl
+
+template <typename Tuple, typename VFunc>
+constexpr decltype(auto) switch_index(Tuple& t, int i, VFunc&& f) {
+    return impl::switch_index<std::tuple_size_v<Tuple> - 1>::switch_case(t, i, std::forward<VFunc>(f));
+}
+
+template <typename T, typename Tuple, typename VFunc>
+constexpr decltype(auto) switch_type(Tuple& t, VFunc&& f) {
+    return switch_index(t, type_index_v<T, Tuple>, std::forward<VFunc>(f));
+}
 
 } // namespace astl::tuple
--- astl/test/t-tuple_util.cpp
+++ astl/test/t-tuple_util.cpp
@@ -14,3 +14,38 @@ TEST_CASE("type_index") {
     static_assert(type_index_v<double, t> == 2);
     static_assert(type_index_v<char, t> == -1);
 }
+
+struct to_int {
+    template <typename T>
+    constexpr int operator()(T t) const {
+        return static_cast<int>(t);
+    }
+
+    constexpr int operator()() {
+        return -45;
+    }
+};
+
+TEST_CASE("index_switch") {
+    std::tuple t = {1, 2.3f, 3.2, 4.1f};
+
+    auto do_switch = [&](int n) {
+        return astl::tuple::switch_index(t, n, to_int{});
+    };
+
+    CHECK(do_switch(0) == 1);
+    CHECK(do_switch(1) == 2);
+    CHECK(do_switch(2) == 3);
+    CHECK(do_switch(3) == 4);
+    CHECK(do_switch(4212) == -45);
+    CHECK(do_switch(-3) == -45);
+}
+
+TEST_CASE("type_switch") {
+    constexpr std::tuple t = { 1, 2.3f, 3.2, 4.1f };
+
+    static_assert(astl::tuple::switch_type<int>(t, to_int{}) == 1);
+    static_assert(astl::tuple::switch_type<float>(t, to_int{}) == 2);
+    static_assert(astl::tuple::switch_type<double>(t, to_int{}) == 3);
+    static_assert(astl::tuple::switch_type<char>(t, to_int{}) == -45);
+}
