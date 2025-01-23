4b8145f6b14ef215bcd97ec11b5c2b12188dc9a8
Borislav Stanimirov <b@ibob.bg>
2025-01-23 20:36:09 +0200

    feat(frameio): add stream, ref #222

---
+++ frame/code/ac/frameio/StreamPtr.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include <memory>
+
+namespace ac::frameio {
+
+class Stream;
+using StreamPtr = std::unique_ptr<Stream>;
+
+class ReadStream;
+using ReadStreamPtr = std::unique_ptr<ReadStream>;
+
+class WriteStream;
+using WriteStreamPtr = std::unique_ptr<WriteStream>;
+
+} // namespace ac::frameio
