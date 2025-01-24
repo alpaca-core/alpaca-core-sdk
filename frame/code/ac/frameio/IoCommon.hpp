06f186c6470b0d776b971dc0a219467b3d4ec074
Borislav Stanimirov <b@ibob.bg>
2025-01-24 09:56:44 +0200

    feat(frameio): use astl::timeout, ref #222

--- frame/code/ac/frameio/IoCommon.hpp
+++ frame/code/ac/frameio/IoCommon.hpp
@@ -11,7 +11,7 @@ template <typename IO>
 struct InputCommon final : public IO, public Input {
     using IO::IO;
     virtual FrameRefWithStatus get(Frame& frame) override { return IO::io(frame); }
-    virtual void poll(Frame& frame, int32_t timeout, IoCb cb) override { IO::io(frame, timeout, std::move(cb)); }
+    virtual void poll(Frame& frame, astl::timeout timeout, IoCb cb) override { IO::io(frame, timeout, std::move(cb)); }
     virtual void close() override { IO::close(); }
 };
 
@@ -19,7 +19,7 @@ template <typename IO>
 struct OutputCommon final : public IO, public Output {
     using IO::IO;
     virtual FrameRefWithStatus put(Frame& frame) override { return IO::io(frame); }
-    virtual void push(Frame& frame, int32_t timeout, IoCb cb) override { IO::io(frame, timeout, std::move(cb)); }
+    virtual void push(Frame& frame, astl::timeout timeout, IoCb cb) override { IO::io(frame, timeout, std::move(cb)); }
     virtual void close() override { IO::close(); }
 };
 
