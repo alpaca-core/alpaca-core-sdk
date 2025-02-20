e00ed9e1e8f55fee71d915a5a40d52da5ced5d37
Borislav Stanimirov <b@ibob.bg>
2025-02-20 21:38:07 +0200

    feat(xec): set/get thread name

---
+++ common/xec/code/ac/xec/thread_name.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) Alpaca Core
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include "api.h"
+
+#include <thread>
+#include <string_view>
+#include <string>
+
+namespace ac::xec {
+// Debug helpers which give names to threads so that they are easily identifiable in the debugger
+// Return 0 on success, non-zero otherwise
+AC_XEC_API int set_thread_name(std::thread& t, std::string_view name);
+AC_XEC_API int set_this_thread_name(std::string_view name);
+AC_XEC_API std::string get_thread_name(std::thread& t);
+AC_XEC_API std::string get_this_thread_name();
+}
