e39071bb93940ec3f37b94a9c1fb12959eca3dd4
Borislav Stanimirov <b@ibob.bg>
2025-04-04 19:19:42 +0300

    fix(io): fix the compilation error fix

--- common/io/code/ac/io/exception.cpp
+++ common/io/code/ac/io/exception.cpp
@@ -6,7 +6,7 @@
 namespace ac::io {
 stream_closed_error::~stream_closed_error() = default;
 
-const char* stream_closed_error::what() const _NOEXCEPT {
+const char* stream_closed_error::what() const noexcept {
     switch (t) {
     case type::input:
         return "input stream closed";
